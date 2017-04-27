e25185d5e8b34d69e4deeb67af8717da218253f6
ccckmit <ccckmit@gmail.com>
2017-04-27 11:22:33 +0800

    add ArraySize for 06/Add

--- 06/add/asm.c
+++ 06/add/asm.c
@@ -29,17 +29,6 @@ void decimal2binary(int d, string binary) {
   hex2binary(hex, binary);  
 }
 
-string codes[]={
-"@2",
-"D=A",
-"@3",
-"D=D+A",
-"@0",
-"M=D"
-};
-
-int codesLen = 6, dTableSize = 8, dSize = 8;
-
 Pair dMap[] = {
   {"", "000"}, {"M", "001"}, {"D", "010"}, {"MD", "011"},
   {"A","100"}, {"AM","101"}, {"AD","110"}, {"AMD","111"}
@@ -58,6 +47,10 @@ Pair cMap[] = {
   {"D|M", "1010101"}
 };
 
+#define arraySize(array) (sizeof(array)/sizeof(array[0]))
+// #define C_SIZE (sizeof(cMap)/sizeof(cMap[0]))
+// #define D_SIZE (sizeof(dMap)/sizeof(dMap[0]))
+
 int find(string key, Pair map[], int len) {
     for (int i=0; i<len; i++) {
         if (strcmp(map[i].key, key)==0)
@@ -73,91 +66,35 @@ string lookup(string key, Pair map[], int len) {
 }
 
 void code2binary(string code, string binary) {
-    if (code[0]=='@') {
-      // A 指令： ＠number
+    if (code[0]=='@') { // A 指令： ＠number
       int number;
       sscanf(code, "@%d", &number);
-//      printf("d=%d\n", d);
       decimal2binary(number, binary);  
-    } else {
-      // C 指令： d = exp
-      // sprintf(binary, "error");
+    } else { // C 指令： d = comp
       char d[10], comp[100];
       sscanf(code, "%[^=]=%s", d, comp);
-//      printf("  d=%s comp=%s\n", d, comp);
-      string dcode = lookup(d, dMap, dSize);
-//      printf("  dcode=%s\n", dcode);
-      string ccode = lookup(comp, cMap, sizeof(cMap)/sizeof(cMap[0]));
-//      printf("  ccode=%s\n", ccode);
+      string dcode = lookup(d, dMap, arraySize(dMap));
+      string ccode = lookup(comp, cMap, arraySize(cMap));
       string jcode = "000";
       sprintf(binary, "111%s%s%s", ccode, dcode, jcode);
     }
 }
 
 void assemble(string fname) {
-  char infile[100], outfile[100], line[100], binary[17];
+  char infile[100], line[100], binary[17];
   sprintf(infile, "%s.asm", fname);
-//  sprintf(outfile, "%s.hack", fname);
   FILE *fp = fopen(infile, "r");
   while (fgets(line, sizeof(line), fp)) {
     char code[100];
     sscanf(line, "%[^/\n\r]", code);
     if (strlen(code)==0) continue;
-//    printf("code=%s len=%lu\n", code, strlen(code));
     code2binary(code, binary);
     printf("%s\n", binary);
   }
   fclose(fp);
 }
 
+// run: ./asm Add > Add.hack
 int main(int argc, char *argv[]) {
   assemble(argv[1]);
 }
-
-/*  
-  char hex[5];
-  char binary[17];
-  for (int i=0;i<codesLen;i++) {
-//    printf("%s\n", codes[i]);
-    code2binary(codes[i], binary);
-    printf("%s\n", binary);
-  }
-*/  
-/*
-@2
-D=A
-@3
-D=D+A
-@0
-M=D
-*/
-
-
-/*
-char *dTable[]  = { "",    "M",   "D",   "MD",  "A",   "AM",  "AD",  "AMD"};
-char *dBinary[] = { "000", "001", "010", "011", "100", "101", "110", "111"};
-
-int find(char *item, char *array[], int len) {
-    for (int i=0; i<len; i++) {
-        if (strcmp(array[i], item)==0)
-            return i;
-    }
-    return -1;
-}
-
-char *lookup(char *item, char *name[], char *value[], int len) {
-   int i = find(item, name, len);
-   if (i==-1) return NULL;
-   return value[i];
-}
-*/
-/*  
-  printf("%x\n", 10);
-  printf("%x\n", 100);
-  sprintf(hex, "%04x", 33);
-  printf("%s\n", hex);
-  hex2binary(hex, binary);
-  printf("%s\n", binary);
-  decimal2binary(33, binary);
-  printf("binary(33)=%s\n", binary);
-*/  
