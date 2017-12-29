90b7def6408bcb22e15ac7c706e77711db519acc
ccckmit <ccckmit@gmail.com>
2017-12-29 15:23:37 +0800

    convert tab to 4 space

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -1,51 +1,42 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Mult.asm
+// #include <stdio.h>
 
-// Multiplies R0 and R1 and stores the result in R2.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+// int main() {
+//    int R0 = 3;
+//    int R1 = 5;
+// =>    int R2 = 0;
+@0
+D=A
+@R2
+M=D 
+//    while (R0 > 0) {
+// => loop:
+(loop)
+// =>    if (R0 <= 0) goto exit1;
+@R0
+D=M
+@exit1
+D; JLE
 
-// Put your code here.
-// R0=3
-//  @3   // 0
-//	D=A  // 1
-//	@R0  // 2
-//	M=D  // 3
-	
-// R1=5
-//	@5   // 4
-//	D=A  // 5
-//	@R1  // 6
-//	M=D  // 7
+// =>  R2 = R2 + R1;
+@R1
+D=M 
+@R2
+M=D+M
 
-// R2 = 0
-	@R2    // 8
-	M = 0  // 9
-	
-// a = R0
-	@R0    // 10
-	D = M  // 11
-	@a     // 12
-	M = D  // 13
-(LOOP)
-//   if (a <= 0) goto EXIT
-	@a     // 14
-	D = M  // 15
-	@EXIT  // 16
-	D; JLE // 17
-	
-//   a=a-1;
-	@a     // 18
-	M = M-1  // 19
-// 	R2 = R2 + R1;
-	@R1    // 20
-	D = M  // 21
-	@R2    // 22 
-	M = D+M  // 23
-// 	goto LOOP
-	@LOOP  // 24
-  0; JMP // 25
-(EXIT)
-  @EXIT  // 26
-	0; JMP // 27
+// =>  R0 = R0 - 1;
+@R0
+M=M-1
+
+//     printf("R0=%d R1=%d R2=%d\n", R0, R1, R2);
+// =>  goto loop;
+@loop
+0;JMP
+
+// => exit1:
+(exit1)
+@exit1
+0;JMP
+//    }
+    
+//     printf("R2=%d\n", R2);
+// }
